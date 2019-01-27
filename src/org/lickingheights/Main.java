17726ca6e7ccfa3df78fc278ac42f2833aed1469
Karen <karen.bandong.2018@student.lhschools.org>
2019-01-27 10:16:12 -0500

    Phase 1 and 2 complete.

---
+++ src/org/lickingheights/Main.java
@@ -0,0 +1,15 @@
+package org.lickingheights;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Phase 1
+        String primaryWord, secondaryWord;
+
+
+        //Phase 2
+        primaryWord= "volley";
+        secondaryWord="ball";
+
+    }
+}

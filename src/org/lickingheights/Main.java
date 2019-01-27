26cfb01e7b0779d992821ef897e055e1deba58ae
Karen <karen.bandong.2018@student.lhschools.org>
2019-01-27 10:19:37 -0500

    Primary Number and word added together.

--- src/org/lickingheights/Main.java
+++ src/org/lickingheights/Main.java
@@ -5,14 +5,18 @@ public class Main {
     public static void main(String[] args) {
 	// Phase 1
         String primaryWord, secondaryWord;
+        int primaryNumber, secondaryNumber;
 
 
         //Phase 2
         primaryWord= "volley";
         secondaryWord="ball";
+        primaryNumber=20;
+        secondaryNumber=4;
 
 
         System.out.println(primaryWord+secondaryWord);
+        System.out.println(primaryWord+primaryNumber);
 
     }
 }
